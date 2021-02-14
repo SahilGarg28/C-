4096d95eb49b190c13dba4f60b5cff99794afd0f
Sahil Garg <69622920+SahilGarg28@users.noreply.github.com>
2021-02-14 22:45:27 +0530

    Create Who_I_Am.cpp

---
+++ Who_I_Am.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"My name is Sahil Garg."<<endl<<"I am First year student at chandigarh univrsity."<<endl<<"I love to do my work Good and in productive way."<<endl<<"As you can see that I made this code for Introduction other than \"Hello world\".";
+
+    return 0;
+}
