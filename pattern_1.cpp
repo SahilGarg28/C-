a0ca496c0a02a2c9d540819865ab76bbf2d6214a
Sahil Garg <69622920+SahilGarg28@users.noreply.github.com>
2021-05-03 15:24:13 +0530

    Create pattern_1.cpp

---
+++ pattern_1.cpp
@@ -0,0 +1,23 @@
+/*
+*                                                                                                                  
+* *                                                                                                                
+* * *                                                                                                              
+* * * *                                                                                                            
+* * * * * 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r,c;
+    for(r=1;r<=5;r++)
+    {
+        for(c=1;c<=r;c++)
+        printf("* ");
+        printf("\n");
+    }
+
+    return 0;
+}
